8bf87ed1003cf44a632dc036219571b092617d11
Fabio Ignacio Torrejon <147105399+fabioo66@users.noreply.github.com>
2023-12-06 14:14:24 -0300

    CADP

--- CADP/Practica 7/ejercicio2.pas
+++ CADP/Practica 7/ejercicio2.pas
@@ -1,97 +1,137 @@
-program ejercicio2repaso;
+program ejercicio2;
 type
-  prango = 1..6;
-  cliente = record
-    codigo: integer;
-    DNI: integer;
-    apellido: string;
-    nombre: string;
-    poliza: prango;  //monto adicional, de acuerdo al código de póliza que tiene contratada.
-    montomensual: real;
-  end;
-  lista = ^nodo;
-  nodo = record
-    dato: cliente;
-    sig: lista;
-  end;
+	prango = 1..6;
+	cliente = record
+		codigo: integer;
+		dni: integer;
+		apellido: string;
+		nombre: string;
+		poliza: prango;
+		monto: real;
+	end;
   
-procedure leercliente(var c:cliente);
+	tabla = array[prango]of real; //se dispone
+  
+	lista = ^nodoLista;
+	nodoLista = record
+		dato: cliente;
+		sig: lista;
+	end;
+  
+procedure leerCliente(var c:cliente);
 begin
-  readln(c.codigo);
-  readln(c.DNI);
-  readln(c.nombre);
-  readln(c.apellido);
-  readln(c.poliza);
-  readln(c.montomensual);
+	writeln('Ingrese el codigo de cliente');
+	readln(c.codigo);
+	if(c.codigo <> 1122)then begin
+		writeln('Ingrese el dni del cliente');
+		c.dni:= random(3333)+1;
+		writeln(c.dni);
+		writeln('Ingrese el apellido');
+		readln(c.apellido);
+		writeln('Ingrese el nombre');
+		readln(c.nombre);
+		writeln('Ingrese el codigo de poliza');
+		c.poliza:= random(6)+1;
+		writeln(c.poliza);
+		writeln('Ingrese el monto');
+		c.monto:= random(5000)*0.135;
+		writeln(c.monto:0:2);
+   end;
 end;
-procedure agregaradelante(var L:lista; c:cliente);
-var 
-  nue:lista;
-begin
-  new(nue);
-  nue^.dato:= c;
-  nue^.sig:= L;
-  L:= nue;
-end;  
-procedure cargarlista(var L:lista);
+
+procedure cargarVector(var v: tabla); //se dispone
 var
-  c:cliente;
+	i: integer;
 begin
-  L:= nil;
-  repeat
-    leercliente(c);
-    agregaradelante(L,c);
-  until(c.codigo = 1122);
-end;  
-function DNI9(num: integer):boolean;
-var
-  resto, cant: integer;
-begin
-  cant:= 0;
-  while(num <> 0)do begin
-    resto:= num mod 10;
-    if(resto = 9)then 
-      cant:= cant + 1;
-    num:= num div 10;
-  end;
-  DNI9:= (cant >= 9);
+	for i:= 1 to 6 do
+	  v[i]:= random(2000)+1;
 end;
-procedure procesar(L: lista; montoadi: real);
-begin 
-  while(L <> nil)do begin
-    writeln(L^.dato.DNI, L^.dato.apellido, L^.dato.nombre, L^.dato.montomensual + montoadi);
-    if(DNI9(L^.dato.DNI))then
-      writeln(L^.dato.apellido, L^.dato.nombre);
-    L:= L^.sig; 
-  end;    
-end;    
-procedure eliminar(var L: lista; num: integer);
-var 
-  ant, act: lista;
-begin
-  act:= L;
-  ant:= L;
-  while(act^.dato.codigo <> num)do begin
-    ant:= act;
-    act:= act^.sig;
-  if(act <> nil)then begin
-    if(act = ant)then
-      L:= act^.sig
-    else
-      ant^.sig:= act^.sig;
-    dispose(act);  
-  end;
-  end;    
-end;
+
+procedure informarTabla(v:tabla); //se dispone
 var
-  L: lista;
-  c: cliente;
-  montoadicional: real;
-  code: integer;
+	i:integer;
 begin
-  cargarlista(L);
-  readln(montoadicional);
-  procesar(L, montoadicional);
-  readln(code);
-  eliminar(L, code);
-end.
+	for i:= 1 to 6 do
+		writeln('En el codigo de poliza ', i, ' El monto adicional es de ', v[i]:0:2);
+end; 
+
+procedure agregarAdelante(var L: lista; c: cliente);
+var
+	nue: lista;
+begin
+	new(nue);
+	nue^.dato:= c;
+	nue^.sig:= L;
+	L:= nue;
+end;
+
+procedure cargarLista(var L: lista);
+var
+	c:cliente;
+begin
+	leerCliente(c);
+	while(c.codigo <> 1122)do begin
+		agregarAdelante(L, c);
+		leerCliente(c);
+	end;
+end;
+
+function alMenosDosNueves(num: integer): boolean;
+var
+	nueves: integer;
+begin
+	nueves:= 0;
+	while(num <> 0)do begin
+		if((num mod 10) = 9)then
+			nueves:= nueves + 1;
+	    num:= num div 10;
+	end;
+	alMenosDosNueves:= nueves >= 2;
+end;
+
+procedure informar(L: lista; v:tabla);
+var
+  montoTotal: real;
+begin
+	while(L <> nil)do begin
+		montoTotal:= L^.dato.monto + v[L^.dato.poliza];
+		writeln('DNI: ', L^.dato.dni, ' Apellido y nombre ', L^.dato.nombre, L^.dato.apellido, ' monto: ', montoTotal:0:2);
+		if(alMenosDosNueves(L^.dato.dni))then
+			writeln('Cliente con al menos dos digitos 9 en el dni -> ', L^.dato.nombre, L^.dato.apellido);
+		L:= L^.sig;
+	end;
+end;
+
+procedure eliminar(var L:lista; codigo: integer);
+var
+  act, ant: lista;
+begin
+	act:= L;
+	while(act <> nil)and(act^.dato.codigo <> codigo)do begin
+		ant:= act;
+		act:= act^.sig;
+	end;
+	if(act <> nil)then begin
+		if(act <> L)then
+			L:= act^.sig
+	    else
+			ant^.sig:= act^.sig;
+		dispose(act);
+	end;
+end;
+
+VAR
+	v:tabla;
+	L: lista;
+	codigo: integer;
+BEGIN
+	randomize;
+	L:= nil;
+	cargarLista(L);
+	cargarVector(v);
+	informarTabla(v);
+	informar(l, v);
+	writeln('Ingrese un codigo de cliente para eliminarlo');
+	readln(codigo);
+	eliminar(l, codigo);
+END.
