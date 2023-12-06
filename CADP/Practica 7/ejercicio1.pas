c5149a5af89c958c2545567fcc941c2dfacf7ae8
Fabio Ignacio Torrejon <147105399+fabioo66@users.noreply.github.com>
2023-12-06 12:18:51 -0300

    CADP

--- CADP/Practica 7/ejercicio1.pas
+++ CADP/Practica 7/ejercicio1.pas
@@ -1,80 +1,154 @@
-program ejercicio11;
+program ejercicio1;
 type
-  alumno = record
-    nro: integer;
-    apellido: string;
-    promedio: real;
-  end;
-  lista= ^nodo;
-  nodo = record
-    dato:alumno;
-    sig:lista;
-  end;
-procedure leeralumno(var a: alumno);
+	crango = 1..5;
+	persona = record
+		dni: integer;
+		nomYApe: string;
+		edad: integer;
+		codigo: crango;
+	end;
+  
+	lista = ^nodoLista;
+	nodoLista = record
+		dato: persona;
+		sig: lista;
+	end;
+  
+	vector = array[crango] of integer;
+  
+procedure leerPersona(var p:persona);
 begin
-  writeln('Ingrese el codigo');
-  readln(a.nro);
-  if(a.nro <> 0)then begin
-    writeln('Ingrese el apellido');
-    readln(a.apellido);
-    writeln('Ingrese el promedio');
-    readln(a.promedio);
-  end;
+	writeln('Ingrese el dni de la persona');
+	readln(p.dni);
+	if(p.dni <> 335)then begin
+		writeln('Ingrese el nombre y apellido');
+		readln(p.nomYApe);
+		writeln('Ingrese la edad de la persona');
+		//readln(p.edad);
+		p.edad:= random(20)+18;
+		writeln(p.edad);
+		writeln('Ingrese el codigo de genero de actuacion');
+		//readln(p.codigo);
+		p.codigo:= random(5)+1;
+		writeln(p.codigo);
+	end;
 end;
-procedure agregarAdelante(var L:lista; a:alumno);
+
+procedure agregarAdelante(var L:lista; p: persona);
 var
-  nue: lista;
+	nue: lista;
 begin
-  new (nue);
-  nue^.dato := a;
-  nue^.sig := L;
-  L:= nue;
+	new(nue);
+	nue^.dato:= p;
+	nue^.sig:= L;
+	L:= nue;
 end;
+
 procedure cargarLista(var L:lista);
 var
-  a: alumno;
-Begin
-  leeralumno(a); 
-  while(a.nro <> 0) do begin
-    agregarAdelante(L, a);
-    leeralumno(a);
-  end;
-end;
-procedure insertarordenado(var L:lista; a:alumno);
-var
-  nue, act, ant:lista;
+	p:persona;
 begin
-  new(nue);
-  nue^.dato:= a;
-  act:= L;
-  ant:= L;
-  while(act <> nil)and(a.promedio < act^.dato.promedio)do begin
-    ant:= act;
-    act:= act^.sig;
-  end;
-  if(act = ant)then
-    L:= nue
-  else 
-    ant^.sig:= nue;
-  nue^.sig:= act;
+	repeat
+		leerPersona(p);
+		agregarAdelante(L, p);
+    until(p.dni = 335);
 end;
-procedure informarpromedios(L: lista);
+
+procedure inicializarVector(var v: vector);
 var
-  i:integer;
-begin 
-  for i:= 1 to 10 do begin 
-    if(L <> nil)then begin
-      writeln(L^.dato.apellido, L^.dato.nro);
-      L:= L^.sig;
-    end;
-  end;
-end;
-var
-  L: lista;
-  a: alumno;
+	i: integer;
 begin
-  L:= nil;
-  cargarLista(L);
-  insertarordenado(L, a);
-  informarpromedios(L);
-end.
+	for i:= 1 to 5 do
+		v[i]:= 0;
+end;
+
+function masPares(num: integer): boolean;
+var
+  pares, impares: integer;
+begin
+	pares:= 0;
+	impares:= 0;
+	while(num <> 0)do begin
+		if((num mod 10) mod 2 = 0)then
+			pares:= pares + 1
+		else
+			impares:= impares + 1;
+		num:= num div 10;
+	end;
+	masPares:= pares > impares;	
+end;
+
+procedure procesarLista(L:lista; var v:vector; var cant: integer);
+begin
+	cant:= 0;
+	while(L <> nil)do begin
+	  if(masPares(L^.dato.dni))then
+	    cant:= cant + 1;
+	  v[L^.dato.codigo]:= v[L^.dato.codigo] + 1;
+	  L:= L^.sig;
+	end;
+end;
+
+procedure codigosMasElegidos(v:vector; var code1, code2: integer);
+var
+	i, max1, max2: integer;
+begin
+	max1:= -9999;
+	max2:= -9999;
+	for i:= 1 to 5 do begin
+	  if(v[i] > max1)then begin
+	    code2:= code1;
+	    max2:= max1;
+	    code1:= i;
+	    max1:= v[i]
+	  end
+	  else
+	    if(v[i] > max2)then begin
+			max2:= v[i];
+			code2:= i;
+	    end;
+	end;
+end;
+
+procedure eliminar(var L:lista; dni: integer; var encontre: boolean);
+var
+  act, ant: lista;
+begin
+	encontre:= false;
+	act:= L;
+	while(act <> nil)and(dni <> act^.dato.dni)do begin
+		ant:= act;
+		act:= act^.sig;
+	end;
+	if(act <> nil)then begin
+		encontre:= true;
+		if(act = L)then
+			L:= act^.sig
+	    else
+			ant^.sig:= act^.sig;
+		dispose(act);
+	end;
+end;
+
+VAR
+	L: lista;
+	v: vector;
+	dni, code1, code2, cant: integer;
+	encontre: boolean;
+BEGIN
+    randomize;
+	L:= nil;
+	cargarLista(L);
+	inicializarVector(v);
+	procesarLista(L, v, cant);
+	writeln('La cantidad de dni con mas pares que impares es ', cant);
+	codigosMasElegidos(v, code1, code2);
+	writeln('Los codigos de genero mas elegidos son ', code1, ' y ', code2);
+	writeln('Ingrese un dni para eliminar a la persona');
+	readln(dni);
+	eliminar(l, dni, encontre);
+	if(encontre)then
+		writeln('El dni fue encontrado y se elimino a esa persona')
+	else
+		writeln('El dni no fue encontrado');
+END.
