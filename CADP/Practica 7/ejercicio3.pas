14271771eb2da61a43b205ee51446288311c6a4e
Fabio Ignacio Torrejon <147105399+fabioo66@users.noreply.github.com>
2023-12-09 19:44:50 -0300

    CADP

--- CADP/Practica 7/ejercicio3.pas
+++ CADP/Practica 7/ejercicio3.pas
@@ -1,80 +1,85 @@
 program ejercicio3;
 type
-  cadena = string[30]
-  viaje = record
-    numero: integer;
-    codigo: integer;
-    direccionori: cadena;
-    direcciondest: cadena;
-    kilometros: real;
-  end;
-  
-  lista = ^nodo;
-  nodo = record
-    dato: viaje;
-    sig: lista;
-  end;
-procedure leerviaje(var v: viaje); //se dispone 
-procedure cargarlista(var L:lista); //se dispone 
+	viaje = record 
+		num: integer;
+		codigo: integer;
+		origen: string;
+		destino: string;
+		km: real;
+	end;
+	
+	lista = ^nodoLista;
+	nodoLista = record
+		dato: viaje;
+		sig: lista;
+	end;
+	
+procedure cargarLista(var L: lista); //se dispone
 
-calcularmaximos(var maxcodigo1, maxcodigo2: integer; var max1, max2: real; km: real; codigo: integer);
-begin
-  if(km > max1)then begin
-    max2:= max1;
-    maxcodigo2:= codigo1;
-    max1:= km;
-    maxcodigo1:= codigo
-  end    
-  else if(km > max2)then begin
-    max2:= km;
-    maxcodigo2:= codigo;
-  end;    
-end;
-procedure agregarviaje(var Lcumple: lista; v: viaje);
+procedure autosMasKm(var code1, code2: integer; km: real; codigo: integer);
 var
-  nue,ant,act: lista;
+	max1, max2: real;
 begin
-  new(nue);
-  nue^.dato:= v;
-  ant:= L;
-  act;
-  while(act <> nil) and (act^.dato.numero < v.numero)do begin
-    ant:= act;
-    act:= act^.sig;
-  end;
-  if(act = ant)then
-    L:= nue;
-  else 
-    ant^.sig:= nue;
-  nue^.sig:= act;    
-end;    
-end;
-procedure procesar(L: lista; var lcumple: lista; var maxcodigo1, maxcodigo2: integer);
-var
-  actcodigo: integer;
-  max1, max2, km: real;
-begin
-  lcumple:= nil;
-  max1:= -9999;
-  maxo2:= -9999;
-  while (L <> nil)do begin
-    km:= 0;
-    actcodigo:= l^.dato.codigo
-    while (L <> nil) and (L^.dato.codigo = actcodigo)do begin
-      km:= km + L^.dato.kilometros;
-      if(L^.dato.kilometros > 5)then
-        agregarviaje(lcumple, l^.dato);
-    L:= L^.sig;
-    end;
-    calcularmaximos(maxcodigo1, maxcodigo2, max1, max2, km, actcodigo);
-  end;    
+	max1:= -9999;
+	max2:= -9999;
+	if(km > max1)then begin
+		max2:= max1;
+		code2:= code1;
+		max1:= km;
+		code1:= codigo
+	end
+	else
+		if(km > max2)then begin
+			max2:= km;
+			code2:= codigo;
+		end;		
 end;
 
+procedure agregarOrdenado(var L2: lista; v: viaje);
 var
-  L, Lcumple: lista;
-  maxcode1, maxcode2: integer;
+	ant, act, nue: lista;
 begin
-  cargarlista(L) //se dispone
-  procesar(L, Lcumple, maxcode1, maxcode2);
-  writeln(maxcode1, maxcode2);
+	new(nue);
+	nue^.dato:= v;
+	act:= L;
+	ant:= L;
+	while(act <> nil)and(v.num < act^.dato.num)do begin
+		ant:= act;
+		act:= act^.sig;
+	end;
+	if(act = ant)then
+		L:= nue
+	else
+		ant^.sig:= nue;
+	nue^.sig:= act;
 end;
+
+procedure procesar(L: lista; var code1, code2: integer; var L2: lista);
+var
+	actual: integer;
+	km: real;
+begin
+	L2:= nil;
+	while(L <> nil)do begin
+		km:= 0;
+		actual:= L^.dato.codigo;
+		while(L <> nil)and(actual = l^.dato.codigo)do begin
+			km:= km + L^.dato.km;
+			if(l^.dato.km > 5)then
+				agregarOrdenado(L2, L^.dato);
+	    end;
+	    L:= L^.sig;
+		autosMasKm(code1, code2, km, actual);
+	end;
+end;
+
+VAR
+	L: lista;
+	L2: lista;
+	code1, code2: integer;
+BEGIN
+	L:= nil;
+	cargarLista(L); //se dispone
+	procesar(L, code1, code2, L2);
+	writeln('Los codigos maximos son ', code1, ' y ', code2);
+END.
