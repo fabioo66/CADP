22f4523a70091e68bdbaa66813b4dc6671bb5fb1
fabioo66 <torrejonfabio@gmail.com>
2024-07-27 17:55:15 -0300

    en proceso

--- Practicas resueltas/Practica 4 parte 2/ejercicio5.pas
+++ Practicas resueltas/Practica 4 parte 2/ejercicio5.pas
@@ -1,65 +1,79 @@
-program ejercicio5practica4p2;
-const
-  dimf = 500;
-type
-  rangodias: 1..31;
-  rangomes: 1..12;
-  rangoanio: 2010..2030;
-  rangocod: 1..2400;
-  rangocat: 'A'..'F';
-  fec = record;
-    dia:rangodias;
-    mes:rangomes;
-    anio: rangoanio;
-  end;
-  cliente = record;
-    fecha: fec;
-    categoria: rangocat;
-    codigo: rangocod;
-  end;
-vectorclientes = array [1..dimf] of cliente;
-vectormes = array [rangomes] of integer;
-vectoranio = array [rangoanio] of integer;
+{La empresa Amazon Web Services (AWS) dispone de la información de sus 500 clientes monotributistas más
+grandes del país. De cada cliente conoce la fecha de firma del contrato con AWS, la categoría del
+monotributo (entre la A y la F), el código de la ciudad donde se encuentran las oficinales (entre 1 y 2400) y
+el monto mensual acordado en el contrato. La información se ingresa ordenada por fecha de firma de
+contrato (los más antiguos primero, los más recientes últimos).
+Realizar un programa que lea y almacene la información de los clientes en una estructura de tipo vector.
+Una vez almacenados los datos, procesar dicha estructura para obtener:
+a. Cantidad de contratos por cada mes y cada año, y año en que se firmó la mayor cantidad de contratos
+b. Cantidad de clientes para cada categoría de monotributo
+c. Código de las 10 ciudades con mayor cantidad de clientes
+d. Cantidad de clientes que superan mensualmente el monto promedio entre todos los clientes.}
 
-procedure leerfecha(var f:fec);
+program ejercicio5;
+const 
+	dimf = 500;
+type
+	mRango = A..F;
+	cRango = 1..2400;
+	dRango : 1..31;
+	mRango : 1..12;
+	
+	fecha = record
+		dia : dRango;
+		mes : mRango;
+		anio : integer;
+	end;
+	
+	cliente = record
+		fech : fecha;
+		categoria : mRango;
+		codigo : cRango;
+		montoMensual : real;
+	end;
+	
+	vector = array[1..dimf] of cliente;
+	contratosMes = array[1..12] of integer;
+	vectorCategorias = array[A..F] of integer;
+	vectorCiudades = array[1..10] of cRango;
+
+	
+	
+procedure leerFecha(var f : fecha);
 begin
-  read(f.dia);
-  read(f.mes);
-  read(f.anio);
+	with f do begin
+		write('Ingrese dia: ');
+		readln(dia);
+		write('Ingrese mes: ');
+		readln(mes);
+		write('Ingrese anio: ');
+		readln(anio);
+	end;
 end;
-procedure leercliente(var c:cliente);
+
+procedure leerCliente(var c : cliente);
 begin
-  leerfecha(c.fecha);
-  read(c.categoria);
-  read(c.codigo);
+	with c do begin
+		leerFecha(fech);
+		write('Ingrese categoria: ');
+		readln(categoria);
+		write('Ingrese codigo de ciudad: ');
+		readln(codigo);
+		write('Ingrese monto mensual: ');
+		readln(montoMensual);
+	end;
 end;
-procedure cargarvector(var v:vectorclientes);
-begin
-  for i:= 1 to 500 do
-    leercliente(v[i]);
-end;
-procedure inicializarmes (var vm:vectormes);
+
+procedure cargarVector(var v : vector);
 var
-  i:rangomes;
+	i : integer;
 begin
-  for i:= 1 to 12 do
-    vm[i]:= 0;
+	for i := 1 to dimf do begin
+		leerCliente(v[i]);
+	end;
 end;
-procedure inicializaranio (var va:vectoranio);
-var
-  i:rangoanio;
-begin
-  for i:= 2010 to 2030 do
-    va[i]:= 0;
-end;
-{a. Cantidad de contratos por cada mes y cada año, y año en que se firmó la mayor cantidad de contratos}
-procedure incisoA(v: vectorclientes; var vm: vectormes; var va: vectoranio; var maximo:rangoanio);
-var
-  mesactual: rangomes;
-  anioactual: rangoanio;
-  i:integer;
-begin
-  for i:= 1 to 500 do begin
-    mesactual:= v[i].fecha.mes;
-    while (mesactual = )
-end.
+
+
+
+
+
